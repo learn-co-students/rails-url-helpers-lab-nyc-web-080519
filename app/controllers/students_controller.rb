36d2cda0b43a62642d9f8cd97dce3d051c8e5a8d
danielahn <93danielahn@gmail.com>
2019-08-28 16:52:11 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,18 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = set_student
+  end
+
+
+  #no need to create a views page!
+  #we are redirecting to the students show page once we activate/deactive the studenrt
+  #in the controller, we are doing the logic of setting @student.active to true or false
+  def activate
+    @student = set_student
+    @student.active = !@student.active
+    @student.save!
+    redirect_to student_path(@student)
   end
 
   private
