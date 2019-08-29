b5be9e59ed059525397504cdba16d2b398ba923a
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-29 08:07:32 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.create(first_name: "Tom", last_name: "McNamee")
+Student.create(first_name: "Act", last_name: "Student", status: true)
